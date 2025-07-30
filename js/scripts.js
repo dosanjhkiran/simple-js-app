d904fcf13f6a09107e8f21c3ea518202ea9aae5b
dosanjhkiran <115619247+dosanjhkiran@users.noreply.github.com>
2025-07-30 21:38:02 +1000

    Refactor to use IIFE with add and getAll functions for pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,42 @@
-// Array 
-let pokemonList = [
-    {
-      name: "Bulbasaur",
-      height: 7,
-      types: ["grass", "poison"]
-    },
-    {
-      name: "Charmander",
-      height: 6,
-      types: ["fire"]
-    },
-    {
-      name: "Squirtle",
-      height: 5,
-      types: ["water"]
-    }
-  ];
-  for (let i = 0; i < pokemonList.length; i++) {
-    let pokemon = pokemonList[i];
-    let output = pokemon.name + " (height: " + pokemon.height + ")";
-    if (pokemon.height > 6) {
-      output += " - Wow, that’s big!";
+let pokemonRepository = (function () {
+  let pokemonList = [
+      {
+        name: "Bulbasaur",
+        height: 7,
+        types: ["grass", "poison"]
+      },
+      {
+        name: "Charmander",
+        height: 6,
+        types: ["fire"]
+      },
+      {
+        name: "Squirtle",
+        height: 5,
+        types: ["water"]
+      }
+    ];
+   
+    // printArrayDetails function declaration
+  function add(pokemon) {
+      pokemonList.push(pokemon);
     }
   
-    document.write(output + "<br>");
-  }
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    let ul = document.getElementById("pokemon-list");
+    let li = document.createElement("li");
+    li.textContent = `${pokemon.name} (Height: ${pokemon.height})`;
+    ul.appendChild(li);
+  });
+  
+  
+  
